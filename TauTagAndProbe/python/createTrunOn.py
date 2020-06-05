199498f6901df2632981bd99e0f1ad14b316ce6f
Christian Veelken <christian.veelken@cern.ch>
2020-06-05 11:55:12 +0300

    - fixed branchnames for event weights - disabled debug output

--- TauTagAndProbe/python/createTrunOn.py
+++ TauTagAndProbe/python/createTrunOn.py
@@ -69,9 +69,17 @@ class TurnOnData:
         self.hist_passed = None
         self.eff = None
 
+##def dumpHistogram(histogram):
+##    for idxBin in range(histogram.GetNbinsX()):
+##        print(" bin #%i: bin-content = %1.2f +/- %1.2f" % (idxBin + 1, histogram.GetBinContent(idxBin + 1), histogram.GetBinError(idxBin + 1)))
+
 def CreateHistograms(input_file, branchname_weight,
                      channels, decay_modes, discr_name, 
-                     working_points, hist_models, label, var, output_file):
+                     working_points, hist_models, label, var):
+    ##print("<CreateHistograms>:")
+    ##print(" input_file = '%s'" % input_file)
+    ##print(" branchname_weight = '%s'" % branchname_weight)
+
     df = ROOT.RDataFrame('events', input_file)
     turnOn_data = {}
     dm_labels = {}
@@ -93,12 +101,12 @@ def CreateHistograms(input_file, branchname_weight,
                 df_ch = df_wp.Filter('pass_{} > 0.5'.format(channel))
                 for model_name, hist_model in hist_models.items():
                     turn_on = TurnOnData()
-                    if(label == "data"):
-                        branchname_weight = args.branchname_weight_data
-                    else:    
-                        branchname_weight = args.branchname_weight_dy_mc
                     turn_on.hist_total = df_wp.Histo1D(hist_model, var, branchname_weight)
+                    ##print("hist_total:")
+                    ##dumpHistogram(turn_on.hist_total)
                     turn_on.hist_passed = df_ch.Histo1D(hist_model, var, branchname_weight)
+                    ##print("hist_passed:")
+                    ##dumpHistogram(turn_on.hist_passed)
                     turnOn_data[dm][wp][channel][model_name] = turn_on
 
     return turnOn_data
@@ -106,7 +114,7 @@ def CreateHistograms(input_file, branchname_weight,
 output_file = ROOT.TFile(args.output + '.root', 'RECREATE')
 input_files = [ args.input_data, args.input_dy_mc ]
 n_inputs = len(input_files)
-branchnames_weight = [ args.branchname_weight_dy_mc, args.branchname_weight_data ] 
+branchnames_weight = [ args.branchname_weight_data, args.branchname_weight_dy_mc ] 
 labels = [ 'data', 'mc' ]
 idx_data = 0
 idx_mc   = 1
@@ -121,12 +129,12 @@ hist_models = {
     'plot': ROOT.RDF.TH1DModel(var, var, len(bins) - 1, array('d', bins)),
     'fit': ROOT.RDF.TH1DModel(var, var, len(bins_fit) - 1, array('d', bins_fit))
 }
-turnOn = [None] * n_inputs
+turnOn = [ None ] * n_inputs
 for input_id in range(n_inputs):
     print("Creating {} histograms...".format(labels[input_id]))
     turnOn[input_id] = CreateHistograms(input_files[input_id], branchnames_weight[input_id], 
                                         channels, decay_modes, 'byDeepTau2017v2p1VSjet',
-                                        working_points, hist_models, labels[input_id], var, output_file)
+                                        working_points, hist_models, labels[input_id], var)
 dm_labels = {}
 for dm in decay_modes:
     if dm == 'all':
